de9c2b515f769cefca303ef798ac5d4521d4e41f
Abhijit <abhijitthander38@gmail.com>
2025-10-16 21:11:45 +0530

    section 1 cus

--- 04-ui-project/src/components/Section1/ImgCards.jsx
+++ 04-ui-project/src/components/Section1/ImgCards.jsx
@@ -4,7 +4,7 @@ const ImgCards = ({ imgUrl, btnText, btnBg }) => {
       style={{ backgroundImage: `url(${imgUrl})` }}
       className={`bg-cover  h-[80%] w-72 rounded-4xl p-4 relative overflow-hidden`}
     >
-      <div className="absolute bottom-0 left-0 w-full h-1/2 p-4 bg-transparent-to-black  flex flex-col">
+      <div className="backdrop-blur-[3px] absolute bottom-0 left-0 w-full h-1/2 p-4 bg-transparent-to-black  flex flex-col">
         <p className="text-zinc-300 font-semibold">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
           nobis laudantium quam!
--- 04-ui-project/src/components/Section1/Navbar.jsx
+++ 04-ui-project/src/components/Section1/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="h-16 flex items-center justify-between p-6">
+    <div className="h-16 flex items-center justify-between p-6 sticky top-0 backdrop-blur-sm z-10">
       <h3 className=" text-white bg-gray-950 py-1 px-4 rounded-full">
         Target Audience
       </h3>
