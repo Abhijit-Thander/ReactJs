95f24cee9f824c6eea550127d5e6f257151508ca
Abhijit <abhijitthander38@gmail.com>
2025-10-28 20:45:56 +0530

    using axios

--- 06-api-calls/src/App.jsx
+++ 06-api-calls/src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import axios from "axios";
 
 function App() {
-  const getData = async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/users");
-    const data = await res.json();
+  //using fetch
+  // const getData = async () => {
+  //   const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  //   const data = await res.json();
+  //   console.log(data);
+  // };
 
-    console.log(data);
+  // using Axios
+
+  const getData = async () => {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+    console.log(res.data);
   };
 
   return (
