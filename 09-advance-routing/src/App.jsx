58fb219e78a558463aa367a9c77e702d95fc82ac
Abhijit <abhijitthander38@gmail.com>
2025-10-31 12:46:07 +0530

    nested routing

--- 09-advance-routing/src/App.jsx
+++ 09-advance-routing/src/App.jsx
@@ -4,6 +4,9 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Product from "./pages/Product";
+import Men from "./pages/Men";
+import Women from "./pages/Women";
+import Kids from "./pages/Kids";
 
 import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
@@ -18,7 +21,11 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/product" element={<Product />} />
+        <Route path="/product" element={<Product />}>
+          <Route path="men" element={<Men />} />
+          <Route path="women" element={<Women />} />
+          <Route path="kids" element={<Kids />} />
+        </Route>
       </Routes>
       <Footer />
     </div>
--- 09-advance-routing/src/pages/Product.jsx
+++ 09-advance-routing/src/pages/Product.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 const Product = () => {
   return (
     <div>
-      <h1>Product Page</h1>
+      <div className="flex justify-center py-4 gap-8 ">
+        <Link to="/product/men" className="font-semibold text-fuchsia-600">
+          Men
+        </Link>
+        <Link to="/product/women" className="font-semibold text-fuchsia-600">
+          Women
+        </Link>
+        <Link to="/product/kids" className="font-semibold text-fuchsia-600">
+          Kids
+        </Link>
+      </div>
+      <Outlet />
     </div>
   );
 };
